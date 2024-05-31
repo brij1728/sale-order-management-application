1a515a9405b0d94bbf1f6feae2d8e049ac62dbb9
Brijesh Kumar <bk10895@gmail.com>
2024-05-31 12:45:45 +0530

    chore: Add Tailwind CSS configuration files and update App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import './App.css';
-
 function App() {
   return (
-    <>
-      <p>React</p>
-    </>
+    <div className='text-center'>
+      <h1 className='text-4xl font-bold underline'>
+        Hello, Tailwind CSS with Vite!
+      </h1>
+      <p className='text-lg mt-4 text-purple-400'>
+        Edit <code>App.tsx</code> and save to reload.
+      </p>
+    </div>
   );
 }
 
