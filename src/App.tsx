31ee7e796e849a5f8c32741212760b3e338c6137
Brijesh Kumar <bk10895@gmail.com>
2024-05-31 19:16:04 +0530

    refactor: update App and routes structure
    - Remove initial greeting message from App.tsx
    - Comment out the HomePage route and set the LoginPage as default route
    - Optimize import statements in routes.tsx for better readability

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,6 @@ import { AppRoutes } from './routes';
 function App() {
   return (
     <div className='text-center'>
-      <h1 className='text-4xl font-bold underline'>
-        Hello, Tailwind CSS with Vite!
-      </h1>
-      <p className='text-lg mt-4 text-purple-400'>
-        Edit <code>App.tsx</code> and save to reload.
-      </p>
       <AppRoutes />
     </div>
   );
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,19 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import {
-  ForgotPasswordPage,
-  HomePage,
-  LoginPage,
-  NotFound,
-  SignUpPage,
-} from '../pages';
+import { ForgotPasswordPage, LoginPage, NotFound, SignUpPage } from '../pages';
 
 export const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<HomePage />} />
+        {/* <Route path='/' element={<HomePage />} /> */}
         <Route path='/signup' element={<SignUpPage />} />
+        <Route path='/' element={<LoginPage />} />
         <Route path='/login' element={<LoginPage />} />
+
         <Route path='*' element={<NotFound />} />
         <Route path='/forgot-password' element={<ForgotPasswordPage />} />
       </Routes>
